4566be9170f72661cdb801e90a8f15989516d91e
Esemplare Simone <simone.esemplare@itsincom.it>
2025-12-01 10:18:51 +0100

    litle changes

--- first/numeri_primi.cpp
+++ first/numeri_primi.cpp
@@ -2,8 +2,8 @@
 
 int isPrime(int n) {
     if (n <= 1) return 0;
-    if (n == 2) return 1;
-    if (n % 2 == 0) return 0;
+    else if (n == 2) return 1;
+    else if (n % 2 == 0) return 0;
     for (int i = 3; i * i <= n; i += 2) {
         if (n % i == 0) return 0;
     }
